138d38cf71389bb770b67aa4d511053d02ef489e
Visual Pivert <visualpivert@gmail.com>
2023-02-18 15:42:27 +0300

    release

--- lib/app.jsx
+++ lib/app.jsx
@@ -4,5 +4,4 @@ import ReactDOM from "react-dom/client"
 function Root (){
     return <div className="salut">Hello</div>
 }
-const r = document.querySelector("#r")
 ReactDOM.createRoot(document.querySelector("#root")).render(<Root />)
